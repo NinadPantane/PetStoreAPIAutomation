cd0fc628caf22effa0cfc5c9c0aaccdc06e7c121
Dell <Dell@DESKTOP-6QB5I7N>
2025-05-14 17:47:16 +0530

    Update jdk version for API project

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -1,6 +1,6 @@
 package api.endpoints;
 
-/*	Swagger URI  ---> https://petstore.swagger.io/#/
+	/*	Swagger URI  ---> https://petstore.swagger.io/#/ [Open in Browser to check swagger details of API]
 	
 	// User Module
 	
@@ -8,21 +8,22 @@ package api.endpoints;
 	Get User (GET):: https://petstore.swagger.io/v2/user/{username}
 	Update User (PUT):: https://petstore.swagger.io/v2/user/{username}
 	Delete User  (DELETE):: https://petstore.swagger.io/v2/user/{username}
-*/	
+	
+	*/
+
 public class Routes {
 
-	public static String base_url="https://petstore.swagger.io/v2";
-	
+	public static String base_url = "https://petstore.swagger.io/v2";
+
 	// User Module
-	
-		public static String post_url=base_url+"/user";
-		public static String get_url=base_url+"/user/{username}";
-		public static String put_url=base_url+"/user/{username}";
-		public static String delete_url=base_url+"/user/{username}";
-		
-		// Store Modules 
-		
-		
-		// Pet Modules 
-		
+
+	public static String post_url = base_url + "/user";
+	public static String get_url = base_url + "/user/{username}";
+	public static String put_url = base_url + "/user/{username}";
+	public static String delete_url = base_url + "/user/{username}";
+
+	// Store Modules
+
+	// Pet Modules
+
 }
